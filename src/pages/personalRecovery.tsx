434347e3061da54bd21b46a3c6dfb1bc6549be96
Richter Brzeski <richter@Richters-MacBook-Pro.local>
2022-06-25 09:58:52 -0700

    add worldcoin widget

--- src/pages/personalRecovery.tsx
+++ src/pages/personalRecovery.tsx
@@ -1,37 +1,33 @@
 import type { NextPage } from "next";
-
+import worldID from "@worldcoin/id";
 import { Box, Text, Heading, Stack, VStack, StackDivider } from "@chakra-ui/react";
 import { FileUploadForm } from "../components/FileUpload";
 
 const PersonalRecovery: NextPage = () => {
-<<<<<<< HEAD
 
-=======
-    return (
-        <Stack
-            as={Box}
-            direction={['column']}
-            align={'center'}
-            textAlign={'center'}
-            spacing={{ base: 8, md: 14 }}
-            py={{ base: 20, md: 36 }}>
-            <Heading
-                fontWeight={200}
-                fontSize={{ base: 'md', sm: '2xl', md: '5xl' }}
-                lineHeight={'110%'}>
-                You lost your key! HAHAHAHA!
-            </Heading>
-{/*                <FileUploadForm/>*/}
-        </Stack>
-    );
->>>>>>> refs/remotes/origin/main
+  worldID.init("world-id-container", {
+    enable_telemetry: true,
+    action_id: "VerifyHuman",
+    signal: "VerifyHuman"
+  });
+
+  useEffect(() => {
+    try {
+      const result = await worldID.enable();
+      console.log("World ID verified successfully:", result);
+    } catch (failure) {
+      console.warn("World ID verification failed:", failure);
+      // Re-activate here so your end user can try again
+    }
+  });
+
   return (
     <VStack
       divider={<StackDivider borderColor='gray.500' />}
       spacing={2}
       align='stretch'
       >
-       
+      <div id="world-id-container"></div>
     </VStack>
     // <Stack
     //   as={Box}
