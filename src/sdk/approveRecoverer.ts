2edbe264b7afaa0601091cf76b0569d6c6a012e9
Joseph-Gross <josephgross157@gmail.com>
2022-06-24 22:15:34 -0400

    Fix type annotations

--- src/sdk/approveRecoverer.ts
+++ src/sdk/approveRecoverer.ts
@@ -1,10 +1,9 @@
-import { Contract, Signer, Result } from "ethers";
+import {Contract, Signer, Transaction} from "ethers";
 
-import { KEYKOVERY_CONTRACT_ADDRESS } from "./constants";
+import {KEYKOVERY_CONTRACT_ADDRESS} from "./constants";
 import KEYKOVERY_ABI from "./abis/Keycovery.json";
 
-export async function approveRecoverer(signer: Signer, lostWalletAddress: string, newWalletAddress: string, friendSignatures: Array<string>): Promise<Result> {
+export async function approveRecoverer(signer: Signer, lostWalletAddress: string, newWalletAddress: string, friendSignatures: Array<string>): Promise<Transaction> {
   let contract = new Contract(KEYKOVERY_CONTRACT_ADDRESS, KEYKOVERY_ABI, signer);
-  let tx = await contract.approveRecoverer(lostWalletAddress, newWalletAddress, friendSignatures);
-  return tx;
+  return await contract.approveRecoverer(lostWalletAddress, newWalletAddress, friendSignatures);
 }
