ae504c73ef4b78d6910b2e3d27693e7a76d590a8
Richter Brzeski <richter@Richters-MacBook-Pro.local>
2022-06-25 11:47:19 -0700

    redownload apeworks

--- contracts/env/bin/activate.fish
+++ contracts/env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/richter/Movies/Projects/KeycoveryContracts/env"
+set -gx VIRTUAL_ENV "/Users/richter/Movies/Projects/key-recovery/contracts/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
