d79073a9abc69eb2909f4579a0872129d2c86057
Richter Brzeski <richter@Richters-MacBook-Pro.local>
2022-06-26 01:39:12 -0700

    deploy new contract

--- src/sdk/constants.ts
+++ src/sdk/constants.ts
@@ -2,7 +2,7 @@ export const KEYKOVERY_CONTRACT_ADDRESS_MUMBAI =
   "0x9efF702aE59c7F6d947253c22e2884375048F283";
 
 export const KEYKOVERY_CONTRACT_ADDRESS_KOVAN =
-  "0x2412F8bE3dFed53c4C69398fc0bD648904E2f584";
+  "0xAa3F2540d244dbE864E75C2E653944ea542704AF";
 
 // Change for whatever chain current contracts are deployed to
 export const CHAIN_STRING = "kovan";
