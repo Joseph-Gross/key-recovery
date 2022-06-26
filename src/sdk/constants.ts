219164a89a6f6982c49866559803e46d1e9fbbb3
Joseph-Gross <josephgross157@gmail.com>
2022-06-26 00:23:43 -0400

    Add more logging and rename file

--- src/sdk/constants.ts
+++ src/sdk/constants.ts
@@ -2,6 +2,6 @@ export const KEYKOVERY_CONTRACT_ADDRESS =
   "0x9efF702aE59c7F6d947253c22e2884375048F283";
 
 // Change for whatever chain current contracts are deployed to
-export const CHAIN_STRING = "mumbai";
+export const CHAIN_STRING = "kovan";
 export const CHAIN_ID = 80001;
 export const KOVAN_CHAIN_ID = 42;
